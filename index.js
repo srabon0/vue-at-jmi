ff3c0693952c825695a1147af704d409d8588e63
srabon0 <srabonemam4@gmail.com>
2022-08-08 17:41:52 +0600

    template added in vue2

--- index.js
+++ index.js
@@ -13,5 +13,15 @@ new Vue({
         shapCreate : function(){
             return jdenticon.toSvg(this.textIcon, 200);
         }
-    }
+    },
+    template:`
+    <div>
+            <h1> {{ title }} </h1>
+            <h2> Shape Creator </h2>
+            <input v-on:input="inputChange" v-model="textIcon">
+            <div>
+                <span v-html="shapCreate"></span>
+            </div>
+        </div>
+    `
 })
